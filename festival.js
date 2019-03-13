25825a9359b9eaf867ee2449e7bc5a5e31e42c5c
sonja-vdlv <sonjavadleve@gmail.com>
2019-03-13 20:57:36 +0100

    fest

---
+++ festival.js
@@ -0,0 +1,53 @@
+'use strict'
+
+//console.log("Dje si, kako si")
+(function () {
+
+function Genre(name){
+    this.name = name;
+    this.getData = function (){
+        return this.name[0].toUpperCase() + this.name[name.length-1].toUpperCase();
+    }
+}
+
+function Movie (title, genre, length){
+    this.title = title;
+    this.genre = genre.getData();
+    this.length = length;
+
+    this.getData = function(){
+        return this.title + ", " + this.length + "min, " + this.genre;
+    }
+}
+
+function Program (date){
+    this.date = date;
+    this.listOfMovies = [];
+    this.totalNofMovies = 0;
+    this.length = Movie.length;
+
+    this.addMovie = function (movie){
+        this.listOfMovies.push(movie);
+        this.totalNofMovies = this.listOfMovies.length;
+    }
+    this.getData = function () {
+        var totalLength = 0;
+        for (var i=0; i<listOfMovies.length; i++) {
+            totalLength += this.length;
+        }
+        return this.date + ", length:" + totalLength + "min";
+        return Movie.getData();
+    }
+}
+
+function Festival (name){
+    this.name = name;
+    this.listOfPrograms = [];
+    this.numberOfMovies = 0; 
+
+    this.addProgram = function(program){
+        this.listOfPrograms.push(program);
+        this.numberOfMovies += program.totalNofMovies;
+    }
+}
+}) ();
